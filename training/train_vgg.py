a060efb762cd26e1f6b9ed50dd6705286c455870
frl <frl1996@mail.ustc.edu.cn>
2019-10-30 13:37:55 +0800

    's'

--- training/train_vgg.py
+++ training/train_vgg.py
@@ -14,8 +14,40 @@ def load_from_h5(root='/gpub/temp/imagenet2012/hdf5/ILSVRC128.hdf5', batch_size=
         img = f['imgs'][1:]
         # img = (f['imgs'][1:] / 255.0).astype(np.float32).transpose([0, 2, 3, 1])
         label = f['labels'][1:]
-    dset = tf.data.Dataset.from_tensor_slices((img, label))
-    dset = dset.shuffle(shuffle_batch_size).batch(batch_size).prefetch(tf.contrib.data.AUTOTUNE)
+    # dset = tf.data.Dataset.from_tensor_slices((img, label))
+    # dset = dset.shuffle(shuffle_batch_size).batch(batch_size).prefetch(tf.contrib.data.AUTOTUNE)
+    num_imgs = len(label)
+    index = np.arange(num_imgs)
+    return img, label, index
+
+
+class np_dataset(object):
+    def __init__(self, root='/gpub/temp/imagenet2012/hdf5/ILSVRC128.hdf5', batch_size=256):
+        print('Loading data root %s into memory...' % root)
+        with h5.File(root, 'r') as f:
+            self.img = f['imgs'][1:]
+            self.label = f['labels'][1:]
+        self.num_imgs = len(self.label)
+        self.index = np.arange(self.num_imgs)
+        self.batch_size = batch_size
+
+    def gen(self):
+        for i in self.index:
+            yield self.img[i]
+
+    def __len__(self):
+        return self.num_imgs
+
+
+def parser_fn(img):
+    return img.transpose([1, 2, 0]).astype(np.float32) / 255.0
+
+
+def build_np_dataset(root, batch_size, gpu_nums):
+    h5_dset = np_dataset(root, batch_size)
+    dset = tf.data.Dataset.from_generator(h5_dset.gen(), tf.float32)
+    dset = dset.map(map_func=parser_fn, num_parallel_calls=3 * gpu_nums).shuffle(len(h5_dset)).batch(
+        batch_size, drop_remainder=True).repeat().prefetch(tf.contrib.data.AUTOTUNE)
     return dset
 
 
@@ -30,7 +62,7 @@ def training_loop(config: Config):
     print("Start task {}".format(config.task_name))
     strategy = tf.distribute.MirroredStrategy()
     print('Loading Imagenet2012 dataset...')
-    dataset = load_from_h5(root=config.h5root, batch_size=config.batch_size)
+    dataset = build_np_dataset(root=config.h5root, batch_size=config.batch_size, gpu_nums=config.gpu_nums)
     dataset = dataset.make_initializable_iterator()
     with strategy.scope():
         global_step = tf.get_variable(name='global_step', initializer=tf.constant(0), trainable=False,
@@ -40,9 +72,10 @@ def training_loop(config: Config):
         # dataset = strategy.experimental_distribute_dataset(dataset)
         # data_iter = dataset.make_initializable_iterator()
         print("Constructing networks...")
-        img = tf.placeholder(tf.float32, [None, 128, 128, 3])
+        img = dataset.get_next()
+        fixed_x = tf.placeholder(tf.float32)
         Encoder = keras.applications.ResNet50(weights=None, classes=config.dim_z)
-        Generator = resnet_biggan.Generator(image_shape=[128,128,3], embed_y=False,
+        Generator = resnet_biggan.Generator(image_shape=[128, 128, 3], embed_y=False,
                                             embed_z=False,
                                             batch_norm_fn=arch_ops.self_modulated_batch_norm,
                                             spectral_norm=True)
@@ -66,6 +99,11 @@ def training_loop(config: Config):
                 with tf.control_dependencies([E_opt]):
                     return tf.identity(e_loss)
         e_loss = compute_loss(train_step, strategy)
+        print("Building eval module...")
+        with tf.init_scope():
+            # IS, FID, eval_sample = compute_eval(eval_step, strategy, eval_z, data_iter)
+            fixed_w = Encoder(fixed_x)
+            fixed_sample = Generator(z=fixed_w, y=None, is_training=False)
         print('Building init module...')
         with tf.init_scope():
             init = [tf.global_variables_initializer(), dataset.initializer]
@@ -81,24 +119,21 @@ def training_loop(config: Config):
             print("Restore generator and discriminator...")
             saver_g.restore(sess, config.restore_g_dir)
             timer.update()
-            img_, _ = sess.run(dataset.get_next())
-            fixed_img = img_
+            fixed_img = sess.run(dataset.get_next())
             save_image_grid(fixed_img, filename=config.model_dir + '/reals.png')
             print("Completing all work, iteration now start, consuming %s " % timer.runing_time_format)
 
             print("Start iterations...")
             for iteration in range(config.total_step):
-                img_, _ = sess.run(dataset.get_next())
-                img_ = (img_ / 255.0).astype(np.float32).transpose([0, 2, 3, 1])
-                e_loss_, lr_ = sess.run([e_loss, learning_rate], {img: img_})
+                e_loss_, lr_ = sess.run([e_loss, learning_rate])
                 if iteration % config.print_loss_per_steps == 0:
                     timer.update()
                     print("step %d, e_loss %f, learning_rate % f, consuming time %s" %
                           (iteration, e_loss_, lr_, timer.runing_time_format))
                 if iteration % config.eval_per_steps == 0:
                     timer.update()
-                    fixed_sample = sess.run(x, {img: fixed_img})
-                    save_image_grid(fixed_sample, filename=config.model_dir + '/fakes%06d.png' % iteration)
+                    fixed_ = sess.run(fixed_sample, {fixed_x: fixed_img})
+                    save_image_grid(fixed_, filename=config.model_dir + '/fakes%06d.png' % iteration)
                 if iteration % config.save_per_steps == 0:
                     saver_e.save(sess, save_path=config.model_dir + '/en.ckpt',
                                  global_step=iteration, write_meta_graph=False)
