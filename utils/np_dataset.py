75ab481f6c39915f0c6810c6d45d5285349e262d
frl <frl1996@mail.ustc.edu.cn>
2019-10-31 13:51:28 +0800

    's'

--- utils/np_dataset.py
+++ utils/np_dataset.py
@@ -16,22 +16,21 @@ class ssgan_sample(object):
 
     def gen(self):
         for i in self.index:
-            yield {'img': self.img[i], 'w': self.w[i]}
+            yield self.img[i], self.w[i]
 
     def __len__(self):
         return self.num_imgs
 
 
-def parser_fn(feature):
-    img = feature['img']
-    w = feature['w']
+def parser_fn(img, w):
     return tf.cast(tf.transpose(img, [1, 2, 0]), tf.float32) / 255.0, w
 
 
 def build_np_dataset(root, batch_size, gpu_nums, dset_name='ssgan_sample'):
     np_dset = {'ssgan_sample': ssgan_sample}[dset_name]
     h5_dset = np_dset(root, batch_size)
-    dset = tf.data.Dataset.from_generator(h5_dset.gen, tf.float32, output_shapes=[3, 128, 128])
+    dset = tf.data.Dataset.from_generator(h5_dset.gen, (tf.float32, tf.float32),
+                                          output_shapes=([3, 128, 128], [120]))
     print('Making tensorflow dataset with length %d' % len(h5_dset))
     dset = dset.map(map_func=parser_fn, num_parallel_calls=3 * gpu_nums).shuffle(len(h5_dset)).batch(
         batch_size, drop_remainder=True).repeat().prefetch(tf.contrib.data.AUTOTUNE)
