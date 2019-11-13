2ff3265d22ebd3c329f0bc50e82b4ab5ccbdb1ca
frl <frl1996@mail.ustc.edu.cn>
2019-11-13 14:39:23 +0800

    's'

--- ReLearning/ReLearning.py
+++ ReLearning/ReLearning.py
@@ -6,50 +6,71 @@ from network.resnet import ImagenetModel
 import tensorflow_datasets as tfds
 import h5py as h5
 from tqdm import tqdm
+from utils import np_dataset as npdt
 import numpy as np
-import tensorflow as tf
 
 
-class ILSVRC128(object):
-    def __init__(self, root='/gpub/temp/imagenet2012/hdf5/ILSVRC128.hdf5', load_in_mem=True):
+EPS = 1e-15
+
+
+class np_dataset(object):
+    def __init__(self, root='/gpub/temp/imagenet2012/hdf5/ILSVRC128.hdf5', batch_size=256, load_in_mem=True):
         print('Loading data root %s into memory...' % root)
         self.root = root
         self.load_in_mem = load_in_mem
-        self.num_imgs = len(h5.File(root, 'r')['labels'])
-        if load_in_mem:
+        self.num_imgs = len(h5.File(root, 'r')['labels']) - 1
+        if self.load_in_mem:
             with h5.File(root, 'r') as f:
                 self.img = f['imgs'][1:]
                 self.label = f['labels'][1:]
-
         self.index = np.arange(self.num_imgs)
         np.random.shuffle(self.index)
+        self.batch_size = batch_size // 4
 
     def gen_from_mem(self):
         for i in self.index:
-            yield self.img[i], self.label[i]
+            yield self.img[i]
 
     def gen_from_file(self):
         for i in self.index:
             with h5.File(self.root, 'r') as f:
-                yield f['imgs'][i + 1], f['labels'][i + 1]
+                yield f['imgs'][i + 1]
+
+    def fixed_sample(self):
+        index = np.random.randint(0, self.num_imgs, self.batch_size)
+        index.sort()
+        if self.load_in_mem:
+            return self.img[list(index)].transpose([0, 2, 3, 1]) / 255.0
+        else:
+            with h5.File(self.root, 'r') as f:
+                return f['imgs'][list(index + 1)].transpose([0, 2, 3, 1]) / 255.0
 
     def __len__(self):
         return self.num_imgs
 
 
-def parser_fn(img, label):
-    return tf.cast(tf.transpose(img, [1, 2, 0]), tf.float32) / 255.0, label
+def parser_fn(img):
+    return tf.cast(tf.transpose(img, [1, 2, 0]), tf.float32) / 255.0
 
 
-def build_np_dataset(root, batch_size, load_in_mem=False, shuffle_buffer_size=10000):
-    h5_dset = ILSVRC128(root, load_in_mem)
-    gen = h5_dset.gen_from_mem if load_in_mem else h5_dset.gen_from_file
-    dset = tf.data.Dataset.from_generator(gen, (tf.float32, tf.int32),
-                                          output_shapes=([128, 128, 3], []))
+def build_np_dataset(root, batch_size, gpu_nums, load_in_mem=True):
+    h5_dset = np_dataset(root, batch_size, load_in_mem)
+    gen = h5_dset.gen_from_mem if h5_dset.load_in_mem else h5_dset.gen_from_file
+    fixed_img = h5_dset.fixed_sample()
+    dset = tf.data.Dataset.from_generator(gen, tf.float32, output_shapes=[3, 128, 128])
     print('Making tensorflow dataset with length %d' % len(h5_dset))
-    dset = dset.shuffle(shuffle_buffer_size).batch(
+    dset = dset.map(map_func=parser_fn, num_parallel_calls=2 * gpu_nums).shuffle(10000).batch(
         batch_size, drop_remainder=True).repeat().prefetch(tf.contrib.data.AUTOTUNE)
-    return dset
+    return dset, fixed_img
+
+
+def compute_loss(train_step, data, strategy):
+    e_loss, r_loss, s_loss, t_loss = strategy.experimental_run_v2(train_step, (data,))
+    mean_e_losses = strategy.reduce(tf.distribute.ReduceOp.MEAN, e_loss, axis=None)
+    mean_r_losses = strategy.reduce(tf.distribute.ReduceOp.MEAN, r_loss, axis=None)
+    mean_s_losses = strategy.reduce(tf.distribute.ReduceOp.MEAN, s_loss, axis=None)
+    mean_t_losses = strategy.reduce(tf.distribute.ReduceOp.MEAN, t_loss, axis=None)
+    return mean_e_losses, mean_r_losses, mean_s_losses,mean_t_losses
 
 
 def training_loop(config: Config):
@@ -58,67 +79,58 @@ def training_loop(config: Config):
     strategy = tf.distribute.MirroredStrategy()
     print('Loading Imagenet2012 dataset...')
     # dataset = load_from_h5(root=config.h5root, batch_size=config.batch_size)
-    dataset = build_np_dataset(root=config.h5root, batch_size=config.batch_size, load_in_mem=config.load_in_mem)
+    dataset, fixed_img = build_np_dataset(root=config.h5root, batch_size=config.batch_size, gpu_nums=config.gpu_nums,
+                                          load_in_mem=config.load_in_mem)
+    dataset = strategy.experimental_distribute_dataset(dataset)
     dataset = dataset.make_initializable_iterator()
     with strategy.scope():
         global_step = tf.get_variable(name='global_step', initializer=tf.constant(0), trainable=False,
                                       aggregation=tf.VariableAggregation.ONLY_FIRST_REPLICA)
-        # dataset = get_dataset(name=config.dataset,
-        #                       seed=config.seed).train_input_fn(params=data_iter_params)
-        # dataset = strategy.experimental_distribute_dataset(dataset)
-        # data_iter = dataset.make_initializable_iterator()
         print("Constructing networks...")
-        # img = tf.placeholder(tf.float32, [None, 128, 128, 3])
         fixed_x = tf.placeholder(tf.float32, [None, 128, 128, 3])
-        img = dataset.get_next()
-        Encoder = ImagenetModel(resnet_size=50, num_classes=120, name='Encoder')
-        VGG_alter = ImagenetModel(resnet_size=50, num_classes=120, name='vgg_alter')
+        Encoder = ImagenetModel(resnet_size=50, num_classes=120, name='vgg_alter')
         Generator = resnet_biggan.Generator(image_shape=[128, 128, 3], embed_y=False,
                                             embed_z=False,
                                             batch_norm_fn=arch_ops.self_modulated_batch_norm,
                                             spectral_norm=True)
-        Discriminator = resnet_biggan.Discriminator(spectral_norm=True, project_y=False)
-        learning_rate = tf.train.exponential_decay(0.0001, global_step, 150000 / config.gpu_nums,
+        learning_rate = tf.train.exponential_decay(config.lr, global_step, 60000,
                                                    0.8, staircase=False)
         E_solver = tf.train.AdamOptimizer(learning_rate=learning_rate, name='e_opt', beta2=config.beta2)
-        # D_solver = tf.train.AdamOptimizer(learning_rate=learning_rate * 5, name='d_opt', beta1=config.beta1)
-
+        G_embed_np = np.load('/ghome/fengrl/ssgan/invSSGAN/G_embed.npy')
+        G_embed = tf.convert_to_tensor(G_embed_np, dtype=tf.float32, name='G_embed')
         print("Building tensorflow graph...")
 
         def train_step(image):
-
+            sample_z = tf.random.normal([config.batch_size // config.gpu_nums, config.dim_z],
+                                        stddev=1.0, name='sample_z')
+            sample_w = tf.matmul(sample_z, G_embed, name='sample_w')
+            sample_img = Generator(sample_w, y=None, is_training=True)
             w = Encoder(image, training=True)
             x = Generator(w, y=None, is_training=True)
-            # _, real_logits, _ = Discriminator(img, y=None, is_training=True)
-            _, fake_logits, _ = Discriminator(x, y=None, is_training=True)
-            # real_logits = fp32(real_logits)
-            fake_logits = fp32(fake_logits)
+            w_ = Encoder(x, training=True)
+            ww_ = Encoder(sample_img, training=True)
             with tf.variable_scope('recon_loss'):
-                recon_loss_pixel = tf.reduce_mean(tf.square(x - image))
-                adv_loss = tf.reduce_mean(tf.nn.softplus(-fake_logits)) * config.g_loss_scale
-                vgg_real = VGG_alter(image, training=True)
-                vgg_fake = VGG_alter(x, training=True)
-                feature_scale = tf.cast(tf.reduce_prod(vgg_real.shape[1:]), dtype=tf.float32)
-                vgg_loss = config.r_loss_scale * tf.nn.l2_loss(vgg_fake - vgg_real) / (
-                            config.batch_size * feature_scale)
-                e_loss = recon_loss_pixel + adv_loss + vgg_loss
-            # with tf.variable_scope('d_loss'):
-            #     d_loss_real = tf.reduce_mean(tf.nn.relu(1.0 - real_logits))
-            #     d_loss_fake = tf.reduce_mean(tf.nn.relu(1.0 + fake_logits))
-            #     d_loss = d_loss_real + d_loss_fake
+                recon_loss_pixel = tf.reduce_mean(tf.square(w_ - w)) / (tf.reduce_mean(tf.square(w)) + EPS)
+                trip_loss = triplet_loss(w_, w, config.triple_margin)
+                sample_loss = tf.reduce_mean(tf.square(ww_ - sample_w)) / (tf.reduce_mean(tf.square(sample_w)) + EPS)
+                e_loss = recon_loss_pixel + sample_loss * config.s_loss_scale + trip_loss
 
             add_global = global_step.assign_add(1)
             update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
             with tf.control_dependencies([add_global] + update_ops):
                 E_opt = E_solver.minimize(e_loss, var_list=Encoder.trainable_variables)
                 with tf.control_dependencies([E_opt]):
-                    return tf.identity(e_loss)
-
-        e_loss = compute_loss(train_step, dataset.get_next(), strategy)
+                    return tf.identity(e_loss), tf.identity(recon_loss_pixel),\
+                           tf.identity(sample_loss), tf.identity(trip_loss)
+        e_loss, r_loss, s_loss, t_loss = compute_loss(train_step, dataset.get_next(), strategy)
         print("Building eval module...")
         with tf.init_scope():
-            fixed_w = Encoder(fixed_x, training=False)
-            fixed_sample = Generator(z=fixed_w, y=None, is_training=False)
+            # def eval_fn():
+            fixed_w = Encoder(fixed_x, training=True)
+            fixed_sample = Generator(z=fixed_w, y=None, is_training=True)
+                # return fixed_sample
+            # fixed_sample = strategy.experimental_run_v2(eval_fn, ())
+
         print('Building init module...')
         with tf.init_scope():
             init = [tf.global_variables_initializer(), dataset.initializer]
@@ -126,35 +138,29 @@ def training_loop(config: Config):
                          and 'beta1_power' not in v.name
                          and 'beta2_power' not in v.name
                          and 'generator' in v.name]
-            restore_d = [v for v in tf.global_variables() if 'opt' not in v.name
-                         and 'beta1_power' not in v.name
-                         and 'beta2_power' not in v.name
-                         and 'discriminator' in v.name]
             saver_g = tf.train.Saver(restore_g, restore_sequentially=True)
-            saver_d = tf.train.Saver(restore_d, restore_sequentially=True)
-            saver_v = tf.train.Saver(VGG_alter.trainable_variables, restore_sequentially=True)
             saver_e = tf.train.Saver(Encoder.trainable_variables, restore_sequentially=True)
         print("Start training...")
         with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as sess:
             sess.run(init)
-            print("Restore generator and discriminator...")
+            print("Restore generator...")
             saver_g.restore(sess, config.restore_g_dir)
-            saver_d.restore(sess, config.restore_d_dir)
-            saver_v.restore(sess, config.restore_v_dir)
-            timer.update()
-            fixed_img = sess.run(dataset.get_next())
+            if config.resume:
+                saver_e.restore(sess, config.restore_v_dir)
             save_image_grid(fixed_img, filename=config.model_dir + '/reals.png')
+            timer.update()
+
             print("Completing all work, iteration now start, consuming %s " % timer.runing_time_format)
 
             print("Start iterations...")
             for iteration in range(config.total_step):
-                e_loss_, adv_loss_, recon_loss_pixel_, vgg_loss_, lr_ = sess.run(
-                    [e_loss, adv_loss, recon_loss_pixel, vgg_loss, learning_rate])
+                e_loss_, r_loss_, s_loss_, t_loss_, lr_ = sess.run(
+                    [e_loss, r_loss, s_loss, t_loss, learning_rate])
                 if iteration % config.print_loss_per_steps == 0:
                     timer.update()
-                    print("step %d, e_loss %f, adv_loss %f, recon_loss_pixel %f, vgg_loss %f, "
+                    print("step %d, e_loss %f, r_loss %f, s_loss %f, t_loss %f "
                           "learning_rate % f, consuming time %s" %
-                          (iteration, e_loss_, adv_loss_, recon_loss_pixel_, vgg_loss_,
+                          (iteration, e_loss_, r_loss_, s_loss_, t_loss_,
                            lr_, timer.runing_time_format))
                 if iteration % config.eval_per_steps == 0:
                     timer.update()
@@ -170,3 +176,15 @@ def fp32(*values):
         values = values[0]
     values = tuple(tf.cast(v, tf.float32) for v in values)
     return values if len(values) >= 2 else values[0]
+
+
+def triplet_loss(w_, w, margin):
+    w_1 = tf.tile(tf.expand_dims(w_, 1), [1, w_.shape[0].value, 1])
+    w1 = tf.tile(tf.expand_dims(w, 0), [w.shape[0].value, 1, 1])
+
+    pairwise_dis = tf.reduce_mean(tf.square(w_1 - w1), axis=2) / (tf.reduce_mean(tf.square(w1), axis=2) + EPS)
+    p_dis = tf.diag_part(pairwise_dis)
+    p_dis = tf.tile(tf.expand_dims(p_dis, 1), [1, p_dis.shape[0].value])
+    loss = tf.reduce_mean(tf.nn.relu(p_dis - pairwise_dis + margin * (
+            tf.ones_like(p_dis) - tf.eye(p_dis.shape[0].value, p_dis.shape[1].value, dtype=p_dis.dtype))))
+    return loss
---
+++ train_rep.py
@@ -0,0 +1,80 @@
+from utils.training_utils import *
+from argparse import ArgumentParser
+from importlib import import_module
+
+
+usage = 'Parser for all sample.'
+parser = ArgumentParser(description=usage)
+parser.add_argument('--model', type=str, default='train_vgg',
+                    help='seed for np')
+parser.add_argument('--load_in_mem', action='store_true', default=False,
+                    help='seed for np')
+parser.add_argument('--resume', action='store_true', default=False,
+                    help='seed for np')
+parser.add_argument('--resume_assgin', action='store_true', default=False,
+                    help='seed for np')
+parser.add_argument('--resume_ebn', action='store_true', default=False,
+                    help='seed for np')
+parser.add_argument('--load_num', type=int, default=None,
+                    help='seed for np')
+parser.add_argument('--disc_iter', type=int, default=2,
+                    help='seed for np')
+parser.add_argument('--batch_size', type=int, default=1024,
+                    help='batch size for sample')
+parser.add_argument('--seed', type=int, default=23,
+                    help='seed for np')
+parser.add_argument('--gpu_nums', type=int, default=4,
+                    help='seed for np')
+parser.add_argument('--eval_per_steps', type=int, default=2000,
+                    help='seed for np')
+parser.add_argument('--save_per_steps', type=int, default=2000,
+                    help='seed for np')
+parser.add_argument('--lr_decay_step', type=int, default=60000,
+                    help='seed for np')
+parser.add_argument('--r_loss_scale', type=float, default=0.4,
+                    help='seed for np')
+parser.add_argument('--lr', type=float, default=0.0001,
+                    help='seed for np')
+parser.add_argument('--g_loss_scale', type=float, default=0.03,
+                    help='seed for np')
+parser.add_argument('--s_loss_scale', type=float, default=0.01,
+                    help='seed for np')
+parser.add_argument('--alpha', type=float, default=0.02,
+                    help='seed for np')
+parser.add_argument('--beta', type=float, default=0.1,
+                    help='seed for np')
+parser.add_argument('--model_dir_root', type=str, default='/gdata1/fengrl/SSGAN',
+                    help='seed for np')
+parser.add_argument('--h5root', type=str, default='/gpub/temp/imagenet2012/hdf5/ILSVRC128.hdf5',
+                    help='seed for np')
+parser.add_argument('--finalize', action='store_true', default=False,
+                    help='seed for np')
+parser.add_argument('--restore_g_dir', type=str, default='/ghome/fengrl/gen_ckpt/gen-0',
+                    help='seed for np')
+parser.add_argument('--restore_d_dir', type=str, default='/ghome/fengrl/disc_ckpt/disc-0',
+                    help='seed for np')
+parser.add_argument('--restore_assgin_dir', type=str, default='/ghome/fengrl/disc_ckpt/disc-0',
+                    help='seed for np')
+parser.add_argument('--restore_v_dir', type=str, default='/gdata1/fengrl/SSGAN/00036-invSSGAN/vgg.ckpt-12000',
+                    help='seed for np')
+parser.add_argument('--restore_ebn_dir', type=str, default='/gdata1/fengrl/SSGAN/00036-invSSGAN/vgg.ckpt-12000',
+                    help='seed for np')
+parser.add_argument('--task_name', type=str, default='invSSGAN',
+                    help='seed for np')
+parser.add_argument('--triple_margin', type=float, default=0.1,
+                    help='seed for np')
+
+
+args = vars(parser.parse_args())
+
+model = import_module('training.' + args['model'])
+
+tf.config.optimizer.set_jit(True)
+
+config = Config()
+config.set(**args)
+config.make_task_dir()
+config.make_task_log()
+config.write_config_and_gin()
+model.training_loop(config=config)
+config.terminate()
