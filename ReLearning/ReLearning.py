6beb4db0e20b2a29c02d455ee281e2956de5a6fe
frl <frl1996@mail.ustc.edu.cn>
2019-11-14 01:20:16 +0800

    's'

--- ReLearning/ReLearning.py
+++ ReLearning/ReLearning.py
@@ -43,6 +43,8 @@ def training_loop(config: Config):
         def train_step(image, label):
             w = Encoder(image, training=True)
             w = Dense(w)
+            print(w)
+            print(label)
             loss = tf.nn.softmax_cross_entropy_with_logits_v2(label, w)
 
             add_global = global_step.assign_add(1)
