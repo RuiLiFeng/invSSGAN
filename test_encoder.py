937b1863fe7405c3701f4436fdf0f1c09dfd3f05
ruilifeng <frl1996@mail.ustc.edu.cn>
2019-11-16 22:18:14 +0800

    's'

--- test_encoder.py
+++ test_encoder.py
@@ -1,9 +1,10 @@
 from utils.training_utils import *
 from argparse import ArgumentParser
 from importlib import import_module
-from network import SSGAN, loss_lib, penalty_lib, tfmetric, resnet_biggan, arch_ops
+from network import SSGAN, loss_lib, penalty_lib, tfmetric, resnet_biggan, arch_ops, resnet_biggan_ssgan
 import logging
 from network.resnet import ImagenetModel
+from training.train_vae_nod_nos_wider_bnplus import assgin_net, BNlayer
 import h5py as h5
 import tensorflow as tf
 
@@ -26,6 +27,10 @@ parser.add_argument('--restore_d_dir', type=str, default='/ghome/fengrl/disc_ckp
                     help='seed for np')
 parser.add_argument('--restore_e_dir', type=str, default='/gdata1/fengrl/SSGAN/00042-train_vgg/vgg.ckpt-48000',
                     help='seed for np')
+parser.add_argument('--restore_a_dir', type=str, default='/gdata1/fengrl/SSGAN/00042-train_vgg/vgg.ckpt-48000',
+                    help='seed for np')
+parser.add_argument('--restore_b_dir', type=str, default='/gdata1/fengrl/SSGAN/00042-train_vgg/vgg.ckpt-48000',
+                    help='seed for np')
 parser.add_argument('--encoder_name', type=str, default='vae_alter',
                     help='seed for np')
 parser.add_argument('--save_name', type=str, default='vae-16000',
@@ -36,11 +41,13 @@ parser.add_argument('--embed_dir', type=str, default='/ghome/fengrl/ssgan/invSSG
 
 args = parser.parse_args()
 
-Encoder = ImagenetModel(resnet_size=50, num_classes=120, name=args.encoder_name)
-Generator = resnet_biggan.Generator(image_shape=[128, 128, 3], embed_y=False,
-                                    embed_z=False,
-                                    batch_norm_fn=arch_ops.self_modulated_batch_norm,
-                                    spectral_norm=True)
+Encoder = ImagenetModel(resnet_size=50, num_classes=None, name=args.encoder_name)
+Generator = resnet_biggan_ssgan.Generator(image_shape=[128, 128, 3], embed_y=False,
+                                          embed_z=False,
+                                          batch_norm_fn=arch_ops.self_modulated_batch_norm,
+                                          spectral_norm=True)
+Assgin_net = assgin_net(x0_ch=512+256, scope='Assgin')
+BN_net = BNlayer(scope='Ebn', z0_ch=1536*16)
 G_embed_np = np.load(args.embed_dir)
 G_embed = tf.convert_to_tensor(G_embed_np, dtype=tf.float32, name='G_embed')
 
@@ -54,10 +61,12 @@ with h5.File(args.h5root, 'r') as f:
     img = f['imgs'][list(index)].transpose([0, 2, 3, 1]) / 255.0
 x = tf.placeholder(tf.float32, [args.batch_size, 128, 128, 3])
 w = Encoder(x, training=True)
-fake_ = Generator(w, y=None, is_training=True)
-fake_ss_ = Generator(wz, y=None, is_training=True)
+w = Assgin_net(w)
+w = BN_net(w, True)
+fake_, _ = Generator(w, y=None, is_training=True)
+fake_ss_, _ = Generator(wz, y=None, is_training=True)
 fake_ss_w = Encoder(fake_ss_, training=True)
-fake_final_ = Generator(fake_ss_w, None, is_training=True)
+fake_final_, _ = Generator(fake_ss_w, None, is_training=True)
 
 with tf.control_dependencies(tf.get_collection(tf.GraphKeys.UPDATE_OPS)):
     fake = tf.identity(fake_)
@@ -74,12 +83,16 @@ restore_g = [v for v in tf.global_variables() if 'opt' not in v.name
              and 'generator' in v.name]
 saver_g = tf.train.Saver(restore_g, restore_sequentially=True)
 saver_e = tf.train.Saver(Encoder.trainable_variables, restore_sequentially=True)
+saver_a = tf.train.Saver(Assgin_net.trainable_variables, restore_sequentially=True)
+saver_b = tf.train.Saver(BN_net.trainable_variables, restore_sequentially=True)
 
 with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as sess:
     sess.run(init)
     print("Restore generator...")
     saver_g.restore(sess, args.restore_g_dir)
     saver_e.restore(sess, args.restore_e_dir)
+    saver_a.restore(sess, args.restore_a_dir)
+    saver_b.restore(sess, args.restore_b_dir)
     fake_img, fake_ss_img, fake_final_img = sess.run([fake, fake_ss, fake_final], {x: img})
     save_image_grid(img, args.save_output_dir + '/' + args.save_name + 'reals_imagenet.png')
     save_image_grid(fake_img, args.save_output_dir + '/' + args.save_name + 'fakes_imagenet.png')
